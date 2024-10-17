1e7705b0d932e67120a041db7a04dc1e92aadacb
Norsuiza <lintoriginal@hotmail.com>
2024-10-16 18:16:52 -0700

    new carousels to pages and buttons

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,14 +2,13 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
+
         <a class="navbar-brand" href="{{ url('/') }}">ToolBelt AI</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
-
-
+        
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -23,5 +22,6 @@
                 </li>
             </ul>
         </div>
+
     </div>
 </nav>
